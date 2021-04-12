5161c845001480cae29eddea6d6e7a47f24f3954
SuperSonic <supersonic@livemail.tw>
2021-04-13 02:28:17 +0800

    Security Update

--- internal/user/access.go
+++ internal/user/access.go
@@ -6,6 +6,7 @@ package user
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/supersonictw/virtual_host-server/internal/auth"
+	"github.com/supersonictw/virtual_host-server/internal/user/fs/middleware"
 )
 
 func NewAccess(c *gin.Context) *auth.Session {
@@ -15,6 +16,11 @@ func NewAccess(c *gin.Context) *auth.Session {
 	}
 	authorization, err := auth.NewAuthorization(accessToken)
 	if err != nil {
+		return nil
+	}
+	identification := authorization.GetIdentification()
+	fullPath := middleware.FullPathExpression("", identification)
+	if middleware.PathTypeDetector(fullPath) != middleware.Directory {
 		return nil
 	}
 	return authorization.GetSession(c)
